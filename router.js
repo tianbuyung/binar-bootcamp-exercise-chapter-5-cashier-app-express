359df2c4758b08e53ca16d0292e8d05ae32f8baf
Septian Maulana <septianm028@gmail.com>
2022-06-28 18:00:44 +0700

    [tianbuyung] - created function to add new menu and added action column to edit/delete menu

--- router.js
+++ router.js
@@ -16,4 +16,25 @@ router.get("/", (req, res) => {
   });
 });
 
+router.post("/add", (req, res) => {
+  fs.readFile("./data.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const name = req.body.name;
+    const price = Number(req.body.price);
+    const quantity = Number(req.body.quantity);
+    const menus = JSON.parse(data);
+    menus.push({
+      name: name,
+      price: price,
+      quantity: quantity,
+      id: menus[menus.length - 1]["id"] + 1,
+      status: true,
+    });
+    fs.writeFile("./data.json", JSON.stringify(menus), (err) => {
+      if (err) throw err;
+      res.redirect("/");
+    });
+  });
+});
+
 module.exports = router;
